ae45c556f042cb86df2806a8f512cdf9d844e7f3
enola-darling <luna.raimondo@gmail.com>
2023-05-03 16:26:40 +0200

    Add

--- 03-05-2023/script-1.js
+++ 03-05-2023/script-1.js
@@ -1,15 +1,4 @@
 
-// Chiedi all'utente di inserire un numero
-const numero = prompt("Inserisci un numero:");
+let numero = prompt("Inserisci un numero:");
 
-// Converte la stringa inserita dall'utente in un numero intero
-const numeroIntero = parseInt(numero);
-
-// Verifica se il numero è pari o dispari utilizzando l'istruzione if-else
-if (numeroIntero % 2 === 0) {
-  console.log("Il numero inserito è pari");
-} else {
-  console.log("Il numero inserito è dispari");
-}
-
-
+(numero % 2 === 0) ? console.log(numero + " è un numero pari.") : console.log(numero + " è un numero dispari.");
--- 03-05-2023/script.js
+++ 03-05-2023/script.js
@@ -1,12 +1,9 @@
 
-// Chiedi all'utente di inserire un numero
-const numero = prompt("Inserisci un numero:");
+let numero = prompt("Inserisci un numero:");
 
-// Converte la stringa inserita dall'utente in un numero intero
-const numeroIntero = parseInt(numero);
-
-// Verifica se il numero è pari o dispari utilizzando l'operatore ternario
-const messaggio = numeroIntero % 2 === 0 ? "Il numero inserito è pari" : "Il numero inserito è dispari";
-
-// Stampa il messaggio nella console
-console.log(messaggio);
+if (numero % 2 === 0) {
+  console.log(numero + " è un numero pari.");
+  
+} else {
+  console.log(numero + " è un numero dispari.");
+}
