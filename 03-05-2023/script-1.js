437c82a3c1487f4d0c1a7ae4e978fcb1ab773dda
enola-darling <luna.raimondo@gmail.com>
2023-05-03 18:05:19 +0200

    add

--- 03-05-2023/script-1.js
+++ 03-05-2023/script-1.js
@@ -1,4 +1,5 @@
 
 let numero = prompt("Inserisci un numero:");
 
-(numero % 2 === 0) ? console.log(numero + " è un numero pari.") : console.log(numero + " è un numero dispari.");
+(numero % 2 === 0) ? console.log(numero + " è un numero pari.") 
+: console.log(numero + " è un numero dispari.");
