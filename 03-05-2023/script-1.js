201153265b37cc70206e724215ed3ed3e32ce380
enola-darling <luna.raimondo@gmail.com>
2023-05-03 19:54:07 +0200

    a better implementation of the ternary operator

--- 03-05-2023/script-1.js
+++ 03-05-2023/script-1.js
@@ -1,5 +1,5 @@
 
-let numero = prompt("Inserisci un numero:");
-(numero % 2 === 0) 
-? console.log(numero + " è un numero pari.") 
-: console.log(numero + " è un numero dispari.");
+let numero = prompt("Inserisci un numero:"); 
+
+console.log(numero + " è un numero " + (numero % 2 === 0 ? "pari." : "dispari."));
+
