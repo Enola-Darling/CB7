c259157ffada3415f8a962880f90d25c5c4b399d
enola-darling <luna.raimondo@gmail.com>
2023-05-04 16:52:47 +0200

    Add script

--- 04-05-2023/script.js
+++ 04-05-2023/script.js
@@ -17,39 +17,7 @@ for (let i = 1; i < numbers.length; i = i + 1) {
 console.log(the_min)
 console.log(the_max)
 
-// 
-/*
-the_min = 10;
 
-i = 1
-i < 11 ? true
-
-0 < 10 ? true
-the_min = numbers[i] = numbers[1] = 0
-
-i++ => i = 2
-2 < 11 ? true
-6 < 0 ? false
-
-i++ => i = 3
-3 < 11 ? true
-9 < 0 ? false
-....
-
-
-i++ => i = 9 
-9 < 11? true
-0 < 0 ? false
-
-i++ => i = 10
-10 < 11 ? true
--255 < 0 ? true
-
-the_min = -255
-
-i++ => i=11
-11 < 11 ? false
-*/
 
 
 let array = [10, 53, 454, 65, 16, 7, 91];
@@ -78,26 +46,3 @@ for (let i = 0; i < Math.floor(array.length / 2); i++) {
 console.log(array);
 
 
-/*
-array = [ 91, 7, 16, 454, 53, 10 ]
-i = 3
-
-aux = 454
-array[i] = array[6 - 1 - i] = array[2] = 16
-array = [ 91, 7, 16, 16, 53, 10 ]
-array[l-1-i] = array[2] = 454
-array = [ 91, 7, 454, 16, 53, 10 ]
-
-i=4
-
-array = [ 91, 53, 454, 16, 7, 10 ]
-
-
-i=5
-
-array = [ 10, 53, 454, 16, 7, 91 ]
-
-
-
-
-*/
