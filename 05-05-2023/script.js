b44a97f3b4c5fe2abe2ae732b9463cabe371fe02
enola-darling <luna.raimondo@gmail.com>
2023-05-07 19:13:15 +0200

    Fixes

--- 05-05-2023/script.js
+++ 05-05-2023/script.js
@@ -29,64 +29,91 @@
 
 /*********** Es: 1 ****************/
 
-// function calc (num1, num2, opt) {
-      
-//     if ( opt === '+') {
-//          return consol.log (num1 + num2); 
-//           } else if (opt === '*') {
-//         return consol.log (num1 * num2); 
-//           } else if ( opt === '-') {
-//             conol.log(num1 - num2);
-//           } else {
-//             consol.log(num1/num2);
-//           } 
-//         }
-
+// function calc_simple(num1, num2, opt) {    
+//     if (opt === '+') {
+//         return num1 + num2; 
+//     } else if (opt === '*') {
+//         return num1 * num2;
+//     } else if (opt === '-') {
+//         return num1 - num2;
+//     } else if (opt === '/') {
+//         return num1 / num2;
+//     } else {
+//     console.error("undefined operator");
+//     }
+// }
 
 /*********** Es: 2 ****************/
 
-function personInfo(obj) {
-   return `${obj.name} è una ${obj.job}`;
-}
+// function personInfo(obj) {
+//    return `${obj.name} è una ${obj.job}`;
+// }
 
-let person = {
-    name: "Luna",
-    surname: "Raimondo",
-    age: "33",
-    job: "designer",
-};
+// let person = {
+//     name: "Luna",
+//     surname: "Raimondo",
+//     age: "33",
+//     job: "designer",
+// };
 
-console.log(personInfo(person));
+// console.log(personInfo(person));
 
 
 
 /*********** Es: Avanzato ****************/
 
-// function calc(num1, num2, opt) {
-//     function somma() {
-//         return console.log(num1 + num2)
-//     }
-//     function moltiplicazione() {
-//         return console.log(num1 * num2)
-//     }
-//     function sottrazione() {
-//         return console.log(num1 - num2)
-//     }
-//     function divisione() {
-//         return console.log(num1 / num2)
-//     }
+/**
+ * Given two numbers, this function calculates the sum.
+ * @param {Number} n1 
+ * @param {Number} n2 
+ * @returns the sum of n1 and n2
+ */
+function somma(n1, n2) {
+    return n1 + n2;
+}
 
-//   if ( opt === '+') {
-//     conole.log(somma());
-//   } else if (opt === '*') {
-//      consol.log(moltiplicazione());
-//   } else if ( opt === '-') {
-//     conol.log(sottrazione());
-//   } else {
-//     consol.log(divisione());
-//   } 
-// }
+function sottrazione(n1, n2) {
+    return n1 - n2;
+}
+
+function moltiplicazione(n1, n2) {
+    return n1 * n2;
+}
+function divisione(n1, n2) {
+    return n1 / n2;
+}
+
+function calc(num1, num2, opt) {
+    switch (opt) {
+        case '+':
+            return somma(num1, num2);
+        case '-':
+            return sottrazione(num1, num2);
+        case '*':
+            return moltiplicazione(num1, num2);
+        case '/':
+            return divisione(num1, num2);
+        default:
+            console.log('undefined operation ' + opt);
+    }    
+}
+
+let output = calc(2, 3, '-') 
+console.log(`output della funzione: ${output}`);
+console.log(calc(2, 4, '%'));
 
 
-
-
+/**
+ * 
+ *  if (opt === '+') {
+        return somma(num1, num2);
+    } else if (opt === '-') {
+        return sottrazione(num1, num2);
+    } else if (opt === '*') {
+        return moltiplicazione(num1, num2);
+    } else if (opt === '/') {
+        return divisione(num1, num2);
+    } else {
+        console.error("undefined operator");
+    }
+ */
