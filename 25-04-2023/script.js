e94a06d716e7cca78d1f3aea75b54bf177e12614
enola-darling <luna.raimondo@gmail.com>
2023-05-26 14:15:37 +0200

    Added new features

--- 25-04-2023/script.js
+++ 25-04-2023/script.js
@@ -7,27 +7,27 @@ const createEl = (type, content, ...attrs) => {
 };
 
 // const createToDoList = () =>{
-// const listModal = createEl("div", "", { name: "class", value: "List_Modal" });
-// const listWrapper = createEl("div", "", {
-//   name: "class",
-//   value: "list_Wrapper",
-// });
-// const addSection = createEl("div", "", { name: "class", value: "add_sectiom" });
-// const AddElement = createEl("input", "", {
-//   name: "type",
-//   value: "text",
-//   name: "placeholder",
-//   value: "Aggiungi elemento",
-// });
-// const button = createEl("button", "Add TODO");
-// const toDoList = createEl("div", "", { name: "class", value: "to_list" });
-// const inputToDo = createEl("input", "", { name: "type", value: "text" });
+const listModal = createEl("div", "", { name: "class", value: "List_Modal" });
+const listWrapper = createEl("div", "", {
+  name: "class",
+  value: "list_Wrapper",
+});
+const addSection = createEl("div", "", { name: "class", value: "add_section" });
+const addInput = createEl("input", "", {
+  name: "type",
+  value: "text",
+  name: "placeholder",
+  value: "Aggiungi elemento",
+});
+const addButton = createEl("button", "Add TODO");
+const cleanUpButton = createEl("button","Clean Up");
+const toDoList = createEl("div", "", { name: "class", value: "to_list" });
 
-// addSection.append(AddElement, button);
-// // toDoList.append(inputToDo, toDoCeckBox);
-// listWrapper.append(addSection, toDoList);
-// listModal.append(addSection, toDoList, listWrapper);
-// document.body.append(listModal);
+addSection.append(addInput, addButton, cleanUpButton);
+// toDoList.append(inputToDo, toDoCeckBox);
+listWrapper.append(addSection, toDoList);
+listModal.append(addSection, toDoList, listWrapper);
+document.body.append(listModal);
 
 // };
 
@@ -43,13 +43,42 @@ fetch(baseUrl)
         name: "class",
         value: "todo_element",
       });
-      const toDoItem = createEl("p", item.todo,{name:"class",value:"todo_item"});
+      const toDoItem = createEl("p", item.todo, {
+        name: "class",
+        value: "todo_item",
+      });
       const toDoCheckBox = createEl("input", "", {
         name: "type",
         value: "checkbox",
       });
       toDoCheckBox.checked = item.completed;
+      toDoCheckBox.dataset.id = item.id;
       toDoElement.append(toDoItem, toDoCheckBox);
       document.body.appendChild(toDoElement);
     });
   });
+
+addButton.addEventListener("click", () => {
+  fetch(`${baseUrl}/add`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      todo: addInput.value,
+      completed: false,
+      userId: 5,
+    }),
+  })
+    .then((res) => res.json())
+    .then(console.log);
+});
+
+
+cleanUpButton.addEventListener("click", ()=> {
+    const checkedCheckboxes = document.querySelectorAll(".todo_element input:checked");
+    checkedCheckboxes.forEach((e) => 
+        fetch(`${baseUrl}/${e.dataset.id}`, {
+          method: 'DELETE',
+        }).then(res => res.json()).then(console.log)
+    );
+});
+
