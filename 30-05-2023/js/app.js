13a3bec1ba0ca8a1cabbdf52e4e86f2123bec95a
enola-darling <luna.raimondo@gmail.com>
2023-05-30 18:08:40 +0200

    twitter-clone

---
+++ 30-05-2023/js/app.js
@@ -0,0 +1,18 @@
+import {tweetGen} from "../utils/fn.js";
+import {GET} from "../utils/http.js";
+const contentEl = document.querySelector(".iconList");
+
+
+let usersList = [];
+
+GET("/users").then(({users}) => usersList = users).then(() => {
+    GET("/post").then(({posts}) => posts.map((p) => {
+        p.user = usersList.find((u) => u.id == p.userId);
+        return p;
+    })).then(posts => posts.forEach(p => {
+        contentEl.append(tweetGen(p));   
+    }));
+});
+
+
+
